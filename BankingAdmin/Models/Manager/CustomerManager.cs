3e2f9552f4fdd05587c7f1a55ee3c01b3d006c00
Shiyu <s3734720@student.rmit.edu.au>
2020-02-01 17:42:34 +1100

    customer manager

---
+++ BankingAdmin/Models/Manager/CustomerManager.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+using BankingAdmin.Data;
+using BankingAdmin.Models;
+using BankingAdmin.Models.Repository;
+
+
+namespace BankingAdmin.Models.Manager {
+    public class CustomerManager : IAsyncRepository<Customer, int>
+    {
+        private readonly BankingContext _context;
+        private readonly DbSet<Customer> _set;
+
+        public CustomerManager(BankingContext context)
+        {
+            _context = context;
+            _set = _context.Customer;
+        }
+
+        public async Task<Customer> GetAsync(int customerId)
+        {
+            return await _set.FindAsync(customerId);
+        }
+
+        public async Task<IEnumerable<Customer>> GetManyAsync(Func<Customer, bool> predicate)
+        {
+            var customers = from customer in _set
+                            where predicate(customer)
+                            select customer;
+            return await customers.ToListAsync();
+        }
+
+        public async Task<IEnumerable<Customer>> GetAllAsync()
+        {
+            return await _set.ToListAsync();
+        }
+
+        public async Task<int> UpdateAsync(int customerId, Customer customer)
+        {
+            customer.CustomerId = customerId;
+            _set.Update(customer);
+            await _context.SaveChangesAsync();
+            return customerId;
+        }
+
+        public async Task<int> DeleteAsync(int customerId)
+        {
+            var customer = await _set.FindAsync(customerId);
+            _set.Remove(customer);
+            await _context.SaveChangesAsync();
+            return customerId;
+        }
+
+        public async Task<int> AddAsync(Customer customer)
+        {
+            await _set.AddAsync(customer);
+            await _context.SaveChangesAsync();
+            return customer.CustomerId;
+        }
+    }
+}
---
+++ BankingAdmin/Models/Repository/IAsyncRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+
+namespace BankingAdmin.Models.Repository
+{
+    public interface IAsyncRepository<TEntity, TKey>
+    {
+        public Task<IEnumerable<TEntity>> GetManyAsync(Func<TEntity, bool> predicate);
+        public Task<IEnumerable<TEntity>> GetAllAsync();
+        public Task<TEntity> GetAsync(TKey id);
+        public Task<TKey> AddAsync(TEntity item);
+        public Task<TKey> UpdateAsync(TKey id, TEntity item);
+        public Task<TKey> DeleteAsync(TKey id);
+    }
+}
