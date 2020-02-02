cc09a81d4e57e4cac660e37af77e2f775875ea94
Shiyu <s3734720@student.rmit.edu.au>
2020-02-03 04:17:41 +1100

    fix: make manager consistent with repo interface

--- BankingAdmin/Models/Repository/IAsyncSearchRepository.cs
+++ BankingAdmin/Models/Repository/IAsyncSearchRepository.cs
@@ -8,7 +8,7 @@ namespace BankingAdmin.Models.Repository
     public interface IAsyncSearchRepository<TEntity, TQuery>
     {
         Task<IEnumerable<TEntity>> GetManyByQueryAsync(TQuery query);
-        Task<IEnumerable<TEntity>> GetManyAsync(Expression<Func<Transaction, bool>>[] predicateList);
+        Task<IEnumerable<TEntity>> GetManyAsync(IEnumerable<Expression<Func<Transaction, bool>>> predicateList);
 
     }
 }
