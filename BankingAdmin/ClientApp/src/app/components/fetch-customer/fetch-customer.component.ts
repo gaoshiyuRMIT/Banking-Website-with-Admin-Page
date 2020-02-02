cdd42d3296e00e4a629dc603945946f3d98fd2b1
Shiyu <s3734720@student.rmit.edu.au>
2020-02-02 23:21:37 +1100

    fetch customer page: search by customerID

--- BankingAdmin/ClientApp/src/app/components/fetch-customer/fetch-customer.component.ts
+++ BankingAdmin/ClientApp/src/app/components/fetch-customer/fetch-customer.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
 import { CustomerService, CustomerData } from '../../services/customer.service';
 
 @Component({
@@ -7,13 +9,30 @@ import { CustomerService, CustomerData } from '../../services/customer.service';
 })
 export class FetchCustomerComponent {
   customerList: CustomerData[];
+  customerIdForm: FormGroup;
 
-  constructor(private _customerService: CustomerService) {
+  constructor(private _customerService: CustomerService, private _fb: FormBuilder) {
     this.getCustomers();
+    this.customerIdForm = _fb.group({
+      customerId: [null, [Validators.max(9999), Validators.min(0)]]
+    });
   }
 
   getCustomers() {
     this._customerService.getCustomers().subscribe(data => this.customerList = data,
       error => console.error(error));
+  }
+
+  search() {
+    if (this.customerId.value !== null)
+      this._customerService.getCustomerById(this.customerId.value).subscribe(
+        data => this.customerList = [data],
+        error => console.error(error));
+    else
+      this.getCustomers();
+  }
+
+  get customerId() {
+    return this.customerIdForm.get("customerId");
   }
 }
