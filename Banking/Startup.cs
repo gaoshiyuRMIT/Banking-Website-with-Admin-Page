1e807c9a40958888676d9dd4021a19a3811d3acb
Shiyu <s3734720@student.rmit.edu.au>
2020-02-03 23:01:28 +1100

    auto logout after 1 min of inactivity of a session

--- Banking/Startup.cs
+++ Banking/Startup.cs
@@ -37,6 +37,7 @@ namespace Banking
             {
                 // Make the session cookie essential.
                 options.Cookie.IsEssential = true;
+                options.IdleTimeout = TimeSpan.FromSeconds(60);
             });
             services.AddScoped<IAccountManager, AccountManager>();
             services.AddScoped<ICustomerManager, CustomerManager>();
