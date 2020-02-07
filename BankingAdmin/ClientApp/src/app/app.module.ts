87e49f3227f7a5ffccdde2a446c3ee812b640532
Shiyu <s3734720@student.rmit.edu.au>
2020-02-07 16:14:55 +1100

    delete built-in example code

--- BankingAdmin/ClientApp/src/app/app.module.ts
+++ BankingAdmin/ClientApp/src/app/app.module.ts
@@ -7,8 +7,6 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
-import { CounterComponent } from './counter/counter.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
 
 import {CustomerService} from './services/customer.service';
 import { FetchCustomerComponent } from './components/fetch-customer/fetch-customer.component';
@@ -26,8 +24,6 @@ import {FetchTransactionComponent} from './components/fetch-transaction/fetch-tr
     AppComponent,
     NavMenuComponent,
     HomeComponent,
-    CounterComponent,
-    FetchDataComponent,
     FetchCustomerComponent,
     FetchCustomerDetailComponent,
     EditCustomerDetailComponent,
@@ -41,8 +37,6 @@ import {FetchTransactionComponent} from './components/fetch-transaction/fetch-tr
     ReactiveFormsModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'counter', component: CounterComponent },
-      { path: 'fetch-data', component: FetchDataComponent },
       { path: 'fetch-customer', component: FetchCustomerComponent },
       {path: 'fetch-customer-detail/:id', component: FetchCustomerDetailComponent},
       {path: 'edit-customer-detail/:id', component: EditCustomerDetailComponent},
