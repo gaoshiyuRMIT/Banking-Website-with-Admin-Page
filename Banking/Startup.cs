17ce78553f6cfe39c451f1992ae05db113e1fd86
Shiyu <s3734720@student.rmit.edu.au>
2020-02-08 14:51:45 +1100

    login session timeout after 1 min

--- Banking/Startup.cs
+++ Banking/Startup.cs
@@ -38,7 +38,7 @@ namespace Banking
             {
                 // Make the session cookie essential.
                 options.Cookie.IsEssential = true;
-                options.IdleTimeout = TimeSpan.FromMinutes(10);
+                options.IdleTimeout = TimeSpan.FromMinutes(1);
             });
             services.AddScoped<IAccountManager, AccountManager>();
             services.AddScoped<ICustomerManager, CustomerManager>();
